ae21816252567c201a2d7f9627e57571d7ab6d78
fantasticsoul <zhongzhengkai@gmail.com>
2022-10-03 21:33:06 +0800

    faet:优化脚本

--- config/subApp.js
+++ config/subApp.js
@@ -2,5 +2,6 @@ const helDevUtils = require('hel-dev-utils');
 const pkg = require('../package.json');
 
 const subApp = helDevUtils.createReactSubApp(pkg, { npmCdnType: 'unpkg' });
+// const subApp = helDevUtils.createReactSubApp(pkg, { npmCdnType: 'unpkg', homePage: 'http://my-unpkg.com' });
 
 module.exports = subApp;
